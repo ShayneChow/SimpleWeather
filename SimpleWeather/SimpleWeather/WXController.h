4d48d3d40ea2f5114f3f9110bb72ff0a627f9624
Shayne Chow <shayne@authbuy.org>
2014-12-31 17:32:18 +0800

    创建CocoaPod管理的workspace，并创建配置新的起始页WXController

---
+++ SimpleWeather/SimpleWeather/WXController.h
@@ -0,0 +1,13 @@
+//
+//  WXController.h
+//  SimpleWeather
+//
+//  Created by choushayne on 14/12/31.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXController : UIViewController
+
+@end
---
+++ SimpleWeather/SimpleWeather/WXController.m
@@ -0,0 +1,38 @@
+//
+//  WXController.m
+//  SimpleWeather
+//
+//  Created by choushayne on 14/12/31.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import "WXController.h"
+
+@interface WXController ()
+
+@end
+
+@implementation WXController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor purpleColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
