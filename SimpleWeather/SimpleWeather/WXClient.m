a8abe099bf362605c95f6f6c6bf711dd9d98a4ec
Shayne Chow <shayne@authbuy.org>
2015-01-04 17:14:26 +0800

    引入ReactiveCocoa

--- SimpleWeather/SimpleWeather/WXClient.m
+++ SimpleWeather/SimpleWeather/WXClient.m
@@ -7,7 +7,23 @@
 //
 
 #import "WXClient.h"
+#import "WXCondition.h"
+#import "WXDailyForecast.h"
+
+@interface WXClient ()
+
+@property (nonatomic, strong) NSURLSession *session;
+
+@end
 
 @implementation WXClient
+
+- (id)init {
+    if (self = [super init]) {
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        _session = [NSURLSession sessionWithConfiguration:config];
+    }
+    return self;
+}
 
 @end
