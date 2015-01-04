c1a17908f641f0a85993ce03f68dbc47ef42dec6
Shayne Chow <shayne@authbuy.org>
2015-01-04 17:55:48 +0800

    返回json数据的主方法

--- SimpleWeather/SimpleWeather/WXClient.m
+++ SimpleWeather/SimpleWeather/WXClient.m
@@ -26,4 +26,45 @@
     return self;
 }
 
+- (RACSignal *)fetchJSONFromURL:(NSURL *)url {
+    NSLog(@"Fetching: %@",url.absoluteString);
+    
+    // 1
+    return [[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        // 2
+        NSURLSessionDataTask *dataTask = [self.session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            if (! error) {
+                NSError *jsonError = nil;
+                id json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&jsonError];
+                if (! jsonError) {
+                    // When JSON data exists and there are no errors, send the subscriber the JSON serialized as either an array or dictionary.
+                    [subscriber sendNext:json];
+                }
+                else {
+                    // If there is an error in either case, notify the subscriber.
+                    [subscriber sendError:jsonError];
+                }
+            }
+            else {
+                // If there is an error in either case, notify the subscriber.
+                [subscriber sendError:error];
+            }
+            
+            // Whether the request passed or failed, let the subscriber know that the request has completed.
+            [subscriber sendCompleted];
+        }];
+        
+        // 3
+        [dataTask resume];
+        
+        // 4
+        return [RACDisposable disposableWithBlock:^{
+            [dataTask cancel];
+        }];
+    }] doError:^(NSError *error) {
+        // 5
+        NSLog(@"%@",error);
+    }];
+}
+
 @end
